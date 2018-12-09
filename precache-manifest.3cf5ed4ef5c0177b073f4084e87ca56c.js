f6d81e98f7e29badb0f2a70761a7da1fc7d0baa8
Dizzy <42190485+dizzy-labs@users.noreply.github.com>
2018-12-08 20:41:16 -0600

    reupload gh-pages to use the production build from the react app

---
+++ precache-manifest.3cf5ed4ef5c0177b073f4084e87ca56c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "0c7a38b263d9331b75c7",
+    "url": "/static/css/main.61e8ce42.chunk.css"
+  },
+  {
+    "revision": "0c7a38b263d9331b75c7",
+    "url": "/static/js/main.0c7a38b2.chunk.js"
+  },
+  {
+    "revision": "64d7528289f9157b9737",
+    "url": "/static/js/1.64d75282.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "300c31475defbb2c693a3afab6237c25",
+    "url": "/index.html"
+  }
+];
