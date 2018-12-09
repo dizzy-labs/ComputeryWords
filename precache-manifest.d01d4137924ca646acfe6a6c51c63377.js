191f10f7f5eef257e881caf31009692481375e03
Dizzy <42190485+dizzy-labs@users.noreply.github.com>
2018-12-08 20:57:44 -0600

    I gotta get a better deployment process for this

---
+++ precache-manifest.d01d4137924ca646acfe6a6c51c63377.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "29ddf85188c256746eb4",
+    "url": "/static/css/main.61e8ce42.chunk.css"
+  },
+  {
+    "revision": "29ddf85188c256746eb4",
+    "url": "/static/js/main.29ddf851.chunk.js"
+  },
+  {
+    "revision": "64d7528289f9157b9737",
+    "url": "/static/js/1.64d75282.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "ac06c2270f514c9e028857c794e10871",
+    "url": "/index.html"
+  }
+];
